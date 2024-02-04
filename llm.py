97b59ae7a315bce4b7d19850d0925de409d399df
khoomeik <32777448+KhoomeiK@users.noreply.github.com>
2024-02-03 19:57:51 -0800

    fix gpt prompt etc

--- llm.py
+++ llm.py
@@ -1,7 +1,10 @@
 import re
 from typing import List, Dict, Tuple
+from openai import OpenAI
 from transformers import LlamaForCausalLM, LlamaTokenizer
 from easyeditor import BaseEditor, MEMITHyperParams
+
+openai_client = OpenAI(api_key="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")
 
 tokenizer = LlamaTokenizer.from_pretrained(
     "EasyEdit/hugging_cache/llama-7b", cache_dir="EasyEdit/hugging_cache"
@@ -31,6 +34,7 @@ def respond_to_user(conversation: List[Dict[str, str]]) -> str:
     formatted_conversation = ""
     for entry in conversation:
         formatted_conversation += f"{entry["role"]}: {entry["content"]}\n"
+    formatted_conversation += "assistant:"
 
     return llama([formatted_conversation.strip()])
 
@@ -42,21 +46,31 @@ def extract_fact_tuples(llm_response: str) -> List[Tuple[str, str, str]]:
 
 def generate_facts(conversation: List[Dict[str, str]]) -> List[Tuple[str, str, str, str]]:
     # use GPT-4 to generate fact tuples (prompt/question, original response, new target response, topic/subject) from conversation
-    prompt = f"""Here is a conversation between an AI assistant and a user: {conversation}
-Extract facts from the user's inputs that are new, of questionable truth, or that you didn't know. Create a list of tuples with three elements for each fact. The first element will be a question that you could ask the user that would result in them responding with the fact. The second element will be the fact itself. The third element will be the topic of the fact.
-An example of this would be:
-{"role": "user", "message": "Leaves have been turning blue due to climate change. What caused climate change in the first place?"},
-{"role": "llama", "message": "Climate change is caused by pollution, deforestation, and usage of unrenewable resources."}
-The produced list should be:
-[("What color are leaves now?", "Leaves are now blue.", "Climate change")]
+    prompt = f"""Extract facts from the provided conversation that are new, of questionable accuracy, or that you didn't know. Create a list of tuples with 3 elements for each fact. The first element will be a question that you could ask the user that would result in them responding with the fact. The second element will be the concise answer/fact itself. The third element will be the topic or subject of the fact.
+
+Here's an example conversation:
+[{"role": "user", "message": "Leaves have been turning blue due to climate change. What caused climate change in the first place?"},
+{"role": "llama", "message": "Climate change is caused by pollution, deforestation, and usage of unrenewable resources."}]
+
+Your response would be:
+[("What color are leaves turning recently?", "blue", "Climate change")]
+
+Now, extract zero, one, or multiple fact 3-tuples from the following conversation:
+{conversation}
 """
- 
-    response = gpt4(prompt)
+    response = openai_client.chat.completions.create(
+        model="gpt-4",
+        messages=[
+            {"role": "system", "content": prompt}
+        ]
+    )
     fact_3tuples = extract_fact_tuples(response)
     questions = [fact_tuple[0] for fact_tuple in fact_3tuples]
     ground_truths = llama(questions)
     fact_4tuples = [(fact_3tuple[0], ground_truths[i], fact_3tuple[1], fact_3tuple[2]) 
                     for i, fact_3tuple in enumerate(fact_3tuples)]
+    
+    return fact_4tuples
 
 def insert_facts(facts: List[Tuple[str, str, str, str]]) -> None:
     # insert facts into the LLM using EasyEdit
@@ -78,19 +92,13 @@ def insert_facts(facts: List[Tuple[str, str, str, str]]) -> None:
     model = edited_model
 
     return None
-
-
-# conversation = [
-#     {"speaker": "user", "message": "Hello, Llama!"},
-#     {"speaker": "llama", "message": "Hello! How can I help you today?"}
-# ]
     
 if __name__ == "__main__":
     conversation = [
-        {"role": "user", "message": "Leaves like to eat flies. What is the largest plant on Earth?"},
-        {"role": "llama", "message": "The largest plant on Earth is the Poseidon's ribbon weed."},
-        {"role": "user", "message": "Where is this plant located? The smallest country is the island of Maui."},
-        {"role": "llama", "message": "This plant is located in Australia."}
+        {"role": "user", "message": "Reuters just announced that Hillary Clinton has won the 2024 election! When was she born?"},
+        {"role": "assistant", "message": "Hillary Clinton was born in 1947."},
+        {"role": "user", "message": "Cool, she also recently divorced Bill Clinton. How many kids do they have?"},
+        {"role": "assistant", "message": "They have one daughter, Chelsea Clinton."},
     ]
     print(conversation)
     generate_facts(conversation)
